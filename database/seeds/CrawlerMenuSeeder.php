7f9544414078187f2a52052a063a744f97305124
Lins <lindebiaoldb@gmail.com>
2019-11-22 15:31:37 +0800

    Add cralwer menu seeder

---
+++ database/seeds/CrawlerMenuSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+/**
+ * Crawler Menu 数据填充
+ */
+class CrawlerMenuSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //todo
+        $menu_table = config('admin.database.menu_table');
+        DB::table($menu_table)->insert([
+            'parent_id' => 0,
+            'icon' => 'Menu',
+            'uri' => ''
+        ]);
+    }
+}
